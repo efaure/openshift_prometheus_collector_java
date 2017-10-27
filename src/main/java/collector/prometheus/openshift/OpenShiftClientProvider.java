70b3b3ece33ee6d59daa533496b373c372b9324d
Eloise Faure <efaure@redhat.com>
2017-10-27 15:01:04 +0200

    Option to add CA to connect to API

--- src/main/java/collector/prometheus/openshift/OpenShiftClientProvider.java
+++ src/main/java/collector/prometheus/openshift/OpenShiftClientProvider.java
@@ -1,11 +1,12 @@
 package collector.prometheus.openshift;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.fabric8.openshift.client.DefaultOpenShiftClient;
 import io.fabric8.openshift.client.OpenShiftClient;
@@ -15,11 +16,14 @@ import io.fabric8.openshift.client.OpenShiftConfigBuilder;
 
 public class OpenShiftClientProvider {
 
+	private static final Logger logger = LoggerFactory.getLogger(OpenShiftClientProvider.class);
+	
 	private static final String OCP_MASTER_URL = "OCP_MASTER_URL";
 	private static final String OCP_USERNAME = "OCP_USERNAME";
 	private static final String OCP_PASSWORD = "OCP_PASSWORD";
 	private static final String OCP_TOKEN = "OCP_TOKEN";
 	private static final String OCP_TOKEN_PATH = "OCP_TOKEN_PATH";
+	private static final String OCP_CA_PATH = "OCP_CA_PATH";
 	private DefaultOpenShiftClient osClient;
 
 	
@@ -29,43 +33,59 @@ public class OpenShiftClientProvider {
 		OpenShiftConfig openshiftConfig;
 		String envToken = getEnvVar(OCP_TOKEN);
 		String envTokenPath = getEnvVar(OCP_TOKEN_PATH);
-		
+		String envCAPath = getEnvVar(OCP_CA_PATH);		
 		String masterUrl = getEnvVar(OCP_MASTER_URL);
-		String token = null;
 		
 		if (masterUrl == null){
 			masterUrl="https://kubernetes.default.svc.cluster.local";
 		}
 		
-		if (envToken != null) token = envToken;
-		else if (envTokenPath != null ) token = readFirstLineFromFile(envTokenPath);
+
+		// Add master URL to builder
+		OpenShiftConfigBuilder openshiftConfigBuilder = new OpenShiftConfigBuilder()
+				.withMasterUrl(masterUrl);
 		
-		if (token != null){			
-			System.out.println("token : "+token);
-			openshiftConfig =  new OpenShiftConfigBuilder()
-					.withMasterUrl(masterUrl)
-					.withOauthToken(token)
-					.build();			
-		}
-				
+		logger.debug("Connection to OpenShift "+masterUrl);
+		
+		
+		// Add token or username password to builder
+		if (envToken != null) {
+			openshiftConfigBuilder = new OpenShiftConfigBuilder()	
+					.withOauthToken(envToken);
+		}			
+		else if (envTokenPath != null ) {
+			String token = readFirstLineFromFile(envTokenPath);
+			openshiftConfigBuilder = new OpenShiftConfigBuilder()
+					.withOauthToken(token);
+		}						
 		else {
 			String username = getEnvVar(OCP_USERNAME);
 			String password = getEnvVar(OCP_PASSWORD);
 			if (username != null && password != null) {
-
-				openshiftConfig =  new OpenShiftConfigBuilder()
-						.withMasterUrl(masterUrl)
+				openshiftConfigBuilder = new OpenShiftConfigBuilder()
 						.withUsername(username)
-						.withPassword(password)
-						.build();
+						.withPassword(password);
 			}
 			else {
 				throw new IllegalArgumentException("Authentication to OpenShift cluster require env var "+OCP_TOKEN+" or "+OCP_TOKEN_PATH+" or ("+OCP_USERNAME+" and "+OCP_PASSWORD+").");
 			}
 		}
+		
+		// Add CA if defined to builder
+		if (envCAPath !=  null){
+			if (new File(envCAPath).isFile() ){
+				openshiftConfigBuilder.withCaCertFile(envCAPath);	
+			}else {
+				throw new IllegalArgumentException(envCAPath+" is not a valid file path for CA");
+			}
+		}
+		
+		// Add master URL to builder
+		openshiftConfig =  openshiftConfigBuilder
+				.build();			
 
 		osClient = new DefaultOpenShiftClient(openshiftConfig);
-		System.out.println("\n namespace "+osClient.getNamespace());
+		logger.debug("Namespace "+osClient.getNamespace());
 	}
  
 	private static OpenShiftClientProvider OCPCLIENTSINGLETON_INSTANCE = null;
@@ -87,18 +107,16 @@ public class OpenShiftClientProvider {
 	private String getEnvVar(String envVarName){
 		String value = System.getenv(envVarName);
 		if (value != null) {
-			System.out.format("%s=%s%n",
-					envVarName, value);
+			logger.info(envVarName+" = "+ value);
 		} else {
-			System.out.format("%s is"
-					+ " not assigned.%n", envVarName);
+			logger.info(envVarName + " is not assigned");
 		}
 		return value;
 	}
 
 
 	public OpenShiftClient getOpenShiftClient() {
-		System.out.println(" \n Getting  openshiftClient");
+		logger.debug("Getting  openshiftClient");
 		return osClient;
 	}
 	
